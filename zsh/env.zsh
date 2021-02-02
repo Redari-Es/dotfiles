d5bae60489206b669eabf104b1414613c091f474
Redari-ES <905422030@qq.com>
2021-02-02 19:03:25 +0800

    env.zsh

---
+++ zsh/env.zsh
@@ -0,0 +1,44 @@
+export XDG_CONFIG_HOME=$HOME/.config
+export LOCALBIN=$XDG_CONFIG_HOME/bin
+export PATH=$PATH:$LOCALBIN
+export LOCALPROG=$HOME/prog
+
+#go
+export GOPATH=$HOME/go
+export GOROOT=/usr/lib/go
+export GOBIN=$HOME/go/bin
+export PATH=$PATH:$GOROOT?bin:$GOBIN
+#export PATH=$PATH:$HOME/go/bin
+
+export PATH=$PATH:$HOME/.gem/ruby/2.7.0
+export PATH=$PATH:$HOME/.gem/ruby/2.7.0/bin
+export PATH=$PATH:/home/redaries/.gem/ruby/2.7.0/bin
+export PATH=$PATH:$HOME/.gem/ruby/:$PATH
+export PATH=$PATH:/usr/local/bin
+
+#pip
+export PATH=/home/redaries/.local/bin/:$PATH
+#brew
+export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
+export PATH=$PATH:/home/linuxbrew/.linuxbrew/sbin
+export PATH=$PATH:$HOME/.linuxbrew/bin
+export PATH=$PATH:$HOME/.linuxbrew/sbin
+export 'HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles'
+export PATH=$PATH:$HOME/.local/bin
+#
+export PATH=$PATH:/usr/local/Cellar/node/14.2.0/bin
+export PATH=$PATH:$HOME/.cargo/bin
+export PATH=$PATH:/snap/bin
+export PATH=$PATH:/usr/local/opt/node@12/bin
+#export TERM=xterm-256color
+export PATH=$PATH:$LOCALPROG/flutter/bin
+export PATH=$PATH:$LOCALPROG/flutter/bin/cache/dart-sdk/bin
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+export TERM=xterm-256color
+export TERM_ITALICS=true
+export RANGER_LOAD_DEFAULT_RC="false"
+#export TERM=screen-256color
+export EDITOR=nvim
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+
